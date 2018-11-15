68490a36ef3385d6f92b76b0088e036f2c94ec89
Daniel Esteves <estevesd8@gmail.com>
2018-11-14 21:30:20 -0400

    carousel, news carousel and footer

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,8 @@
+// Initizalizing Side Navigation
 $(".button-collapse").sideNav();
 
 var sideNavScrollbar = document.querySelector('.custom-scrollbar');
 Ps.initialize(sideNavScrollbar);
+
+// Initizalizing Carousel
+$('.carousel').carousel()
