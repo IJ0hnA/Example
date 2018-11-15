653236acfb60be949fa8525bfed5f330cdaa8bb1
Daniel Esteves <estevesd8@gmail.com>
2018-11-15 14:09:24 -0400

    fixing design of log in

--- iniciar-sesion.php
+++ iniciar-sesion.php
@@ -11,7 +11,7 @@
 <body>
     <?php include './php/includes/menu.php' ?>
 
-    <main>
+    <main class="position-relative">
         <div class="container py-5">
             <div class="row justify-content-center">
                 <div class="col-12 col-md-6 col-lg-4">
